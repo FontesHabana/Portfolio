59e98a723489010bf025e55f88f02d9a80162e28
Javier Fontes <javierfontbas@gmail.com>
2025-12-25 21:37:11 -0500

    feat: refactor main responsive desing and add work page

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -100,7 +100,7 @@ export default function Dropdown() {
               </div>
             </a>
             <a
-              href="#"
+              href="./projects"
               className="py-1 my-1 relative h-12 overflow-hidden text-black hover:bg-blue-100 hover:scale-105 rounded-full transition-colors"
             >
               <div className="px-4   flex flex-col transition-transform duration-500 hover:-translate-y-1/2">
